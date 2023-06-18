6858876f6c2c74357af2c10a3514a5262d8cb857
Leonardo Nicola <leonardonicolares@gmail.com>
2023-06-17 23:09:11 -0300

    fix: import root config nestjs for dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { DrinkModule } from './infra/modules/drink/drink.module';
 import { CommentModule } from './infra/modules/comment/comment.module';
 import { AuthModule } from './infra/modules/auth/auth.module';
 import { UserModule } from './infra/modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, DrinkModule, CommentModule, AuthModule],
+  imports: [
+    UserModule,
+    DrinkModule,
+    CommentModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
