ca5f747d66097b9fecf1f925e5b06ba263ad7fbd
Leonardo Nicola <leonardonicolares@gmail.com>
2023-06-17 23:08:42 -0300

    feat: add AWS SDK for S3 usage

---
+++ src/infra/aws/bucket.repository.ts
@@ -0,0 +1,30 @@
+import { S3 } from 'aws-sdk';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FileUploadService {
+  async upload(file: Express.Multer.File) {
+    const { originalname } = file;
+    const bucketS3 = process.env.S3_BUCKET;
+    return await this.uploadS3(file.buffer, bucketS3, originalname);
+  }
+
+  private async uploadS3(file, bucket, name): Promise<string> {
+    const s3 = new S3({
+      region: process.env.AWS_DEFAULT_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+    const uploadResult = await s3
+      .upload({
+        Bucket: bucket,
+        Body: file,
+        Key: name,
+        ContentType: 'image/png',
+      })
+      .promise();
+    return uploadResult.Location;
+  }
+}
