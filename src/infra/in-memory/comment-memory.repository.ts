3396da2dd01c4e5d5c1e42d5c3c9d36221fb9c71
Leonardo Nicola <leonardonicolares@gmail.com>
2023-06-21 01:51:07 -0300

    feat: add more features to in-memory repo

--- src/infra/in-memory/comment-memory.repository.ts
+++ src/infra/in-memory/comment-memory.repository.ts
@@ -7,6 +7,10 @@ export class InMemoryCommentRepository implements CommentRepository {
   private comments: Comment[] = [];
 
   async createComment(comment: Comment): Promise<Comment> {
+    if (!comment.text) {
+      throw new Error('Comment must have a text');
+    }
+
     const newComment = {
       ...comment,
       id: (this.comments.length + 1).toString(),
@@ -21,9 +25,12 @@ export class InMemoryCommentRepository implements CommentRepository {
 
   async removeComment(id: string): Promise<Comment> {
     const index = this.comments.findIndex((comment) => comment.id === id);
-    if (index !== -1) {
-      const [removedComment] = this.comments.splice(index, 1);
-      return removedComment;
+    if (index === -1) {
+      throw new Error('Comment not found');
     }
+
+    const removedComment = this.comments[index];
+    this.comments.splice(index, 1);
+    return removedComment;
   }
 }
