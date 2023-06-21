5043f53b3e92b889a75e8f555f7d994d13d5bbe7
Leonardo Nicola <leonardonicolares@gmail.com>
2023-06-21 13:19:17 -0300

    fix: remove comments from entity

--- src/core/domain/entities/user.entity.ts
+++ src/core/domain/entities/user.entity.ts
@@ -1,9 +1,7 @@
 import { Entity } from '../../../core/base/entity';
-import { Comment } from './comment.entity';
 
 export interface User extends Entity {
   name: string;
   email: string;
   password: string;
-  comments?: Comment[];
 }
